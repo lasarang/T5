53bbdec0b65685f3c5e5ecf1b6292264dab358b8
MauricioOrtiz1999 <73213899+MauricioOrtiz1999@users.noreply.github.com>
2022-07-01 20:46:50 -0500

    Login test cases added

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 import { LoginComponent } from './login.component';
 
@@ -23,4 +24,45 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it(' Login successful ', () =>{
+    component.email ='mauricioortizlascano@gmail.com';
+    component.password = '12345678';
+
+    const btn = fixture.debugElement.query(By.css('button.btn-lg'));
+    btn.nativeElement.click();
+
+    expect(component.unsuccessAlert).toBeFalse();
+  });
+
+  it(' Failed login, worng password ', () =>{
+    component.email ='mauricioortizlascano@gmail.com';
+    component.password = '12345679';
+
+    const btn = fixture.debugElement.query(By.css('button.btn-lg'));
+    btn.nativeElement.click();
+
+    expect(component.unsuccessAlert).toBeTrue();
+  });
+
+  it(' Failed login, invalid email ', () =>{
+    component.email ='@gmail.com';
+    component.password = 'error123';
+
+    const btn = fixture.debugElement.query(By.css('button.btn-lg'));
+    btn.nativeElement.click();
+
+    expect(component.unsuccessAlert).toBeTrue();
+  });
+
+  it(' Failed login, email not registered ', () =>{
+    component.email ='noregistered@gmail.com';
+    component.password = 'error123';
+
+    const btn = fixture.debugElement.query(By.css('button.btn-lg'));
+    btn.nativeElement.click();
+
+    expect(component.unsuccessAlert).toBeTrue();
+  });
+  
 });
