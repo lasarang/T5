42343de72deed90f10d98218a7e66f1e4a6cd047
Joyce <jerojas@espol.edu.ec>
2022-07-02 23:34:28 -0500

    ID Test

--- src/app/pages/lab-results/lab-results.component.spec.ts
+++ src/app/pages/lab-results/lab-results.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { LabResultsComponent } from './lab-results.component';
 
 describe('LabResultsComponent', () => {
@@ -20,5 +20,35 @@ describe('LabResultsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Empty fields', () => {
+    const btn = fixture.debugElement.query(By.css('button.btn-lg'));
+    btn.nativeElement.click();
+  });
+
+  it('Must not have special characters', () => {
+    component.idOrden = 'Ue*f1&oBNz!2Q5L¿FbP°x';
+    const btn = fixture.debugElement.query(By.css('button.btn-lg'));
+    btn.nativeElement.click();
+  });
+
+  it('Must not have blank spaces', () => {
+    component.idOrden = '  UeSf1BNz  f2Q5LtFb';
+    const btn = fixture.debugElement.query(By.css('button.btn-lg'));
+    btn.nativeElement.click();
+  });
+
+  it('Length must be 20 characters', () => {
+    component.idOrden = 'UeSf13oBNzf2Q5LtFbPxNNNNNNNNNNNNNNNNNN';
+    const btn = fixture.debugElement.query(By.css('button.btn-lg'));
+    btn.nativeElement.click();
+  });
+
+  it('Valid idOrder', () => {
+    component.idOrden = 'UeSf13oBNzf2Q5LtFbPx';
+    const btn = fixture.debugElement.query(By.css('button.btn-lg'));
+    btn.nativeElement.click();
+    expect(component.successAlert).toBeTrue();
   });
 });
